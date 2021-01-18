3a5a11837712679e8e996c450d3bd69bb2041546
Changez Shaikh <changezshaikh@gmail.com>
2021-01-18 16:11:14 +1100

    Creates placeholder for property listings

---
+++ src/components/property-listings/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const PropertyListings = () => {
+  return (
+    <section className="property-listings">
+      <div className="container">
+        <div className="column">
+          <div className="row">
+            <h2>Results</h2>
+          </div>
+          <div className="row">Results go here.</div>
+        </div>
+        <div className="column">
+          <div className="row">
+            <h2>Saved properties</h2>
+          </div>
+          <div className="row">Saved properties go here.</div>
+        </div>
+      </div>
+    </section>
+  );
+};
