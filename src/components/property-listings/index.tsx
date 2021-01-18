b56ec183a7e82a834161ac1599e5582dc2e7f82d
Changez Shaikh <changezshaikh@gmail.com>
2021-01-18 18:32:01 +1100

    Defines a central state and wires up the save and remove functions

--- src/components/property-listings/index.tsx
+++ src/components/property-listings/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropertyTile from "../property-tile";
 import { PROPERTY_TYPES } from "../../shared/constants";
 import { Property } from "../../types/PropertyTypes.d";
@@ -9,18 +9,39 @@ type Props = {
 };
 
 const PropertyListings = ({ results, saved }: Props) => {
+  const [resultProperties, setResultProperties] = useState<Property[]>([]);
+  const [savedProperties, setSavedProperties] = useState<Property[]>([]);
+
+  // update the properties if props change
+  useEffect(() => {
+    setResultProperties(results);
+    setSavedProperties(saved);
+  }, [results, saved]);
+
   const saveProperty = (property: Property) => {
     // saves the property
+    // do nothing if the property already exists as a saved property
+    if (savedProperties.find((item) => item.id === property.id)) return;
+    // Add the new property into the saved properties list
+    setSavedProperties([...savedProperties, property]);
   };
 
   const removeProperty = (property: Property) => {
     // removes the property
+    // do nothing if the property does not exist in the list
+    savedProperties.forEach((item) => {
+      if (item.id === property.id) return;
+    });
+    // Remove the property from the saved list
+    setSavedProperties([...saved.filter((item) => item.id !== property.id)]);
   };
 
   // returns a property list of either saved or result properties
   const renderPropertyList = (propertyType: string) => {
     const properties =
-      propertyType === PROPERTY_TYPES.RESULTS ? results : saved;
+      propertyType === PROPERTY_TYPES.RESULTS
+        ? resultProperties
+        : savedProperties;
 
     if (!properties || !properties.length) return "";
     return properties.map((property) => {
