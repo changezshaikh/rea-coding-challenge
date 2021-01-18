2d502e5ab562bc193ccc699dffca73c21742ef69
Changez Shaikh <changezshaikh@gmail.com>
2021-01-18 16:56:48 +1100

    Style the logo and property images

--- src/components/property-tile/index.tsx
+++ src/components/property-tile/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.scss";
 import { Property } from "../../types/PropertyTypes.d";
 
 type Props = {
@@ -8,8 +9,18 @@ type Props = {
 const PropertyTile = ({ property }: Props) => {
   return (
     <div className="property__container">
-      <div className="property-header">Logo goes here</div>
-      <img src={property.mainImage} alt={property.id} />
+      <div className="property-header">
+        <img
+          src={property.agency.logo}
+          alt="Agency Logo"
+          className="logo-image"
+        />
+      </div>
+      <img
+        src={property.mainImage}
+        alt={property.id}
+        className="property-image"
+      />
     </div>
   );
 };
