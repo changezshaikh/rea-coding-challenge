14c78956c0b48eae4fa1c6a9a75fee42fd2a6322
Changez Shaikh <changezshaikh@gmail.com>
2021-01-18 16:27:08 +1100

    Adds a basic test for property listings

---
+++ src/components/property-listings/index.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import PropertyListings from ".";
+import properties from "../../assets/data/properties.json";
+
+describe("Property Listings tests", () => {
+  test("should render without crashing and match snapshot", () => {
+    const { container } = render(<PropertyListings {...properties} />);
+  });
+});
