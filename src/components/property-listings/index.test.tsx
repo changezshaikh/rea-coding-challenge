a7123ade692acad8cb7f0b7f128f21dd1ed42316
Changez Shaikh <changezshaikh@gmail.com>
2021-01-19 21:45:28 +1100

    Refines the test case to be more specific and use test ids instead of class names

--- src/components/property-listings/index.test.tsx
+++ src/components/property-listings/index.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import PropertyListings from ".";
 import properties from "../../assets/data/properties.json";
 
@@ -9,30 +9,47 @@ describe("Property Listings tests", () => {
     expect(container).toMatchSnapshot();
   });
 
-  test("should correctly add the property to the saved property list", () => {
-    const { container } = render(<PropertyListings {...properties} />);
+  test("should correctly add the first property to the saved property list", () => {
+    render(<PropertyListings {...properties} />);
     // click "Save" on first property
-    screen.getAllByText("Save")[0].click();
-    const saved = container.querySelectorAll(".property-tile-saved");
+    within(screen.getByTestId("result-property-1"))
+      .getByRole("button", { name: /save/i })
+      .click();
     // expect saved list to have another property added
-    expect(saved.length).toBe(2);
+    expect(screen.getByTestId("saved-property-1")).toBeInTheDocument();
+  });
+
+  test("should correctly add a random property to the saved property list", () => {
+    render(<PropertyListings {...properties} />);
+    const randomId = Math.floor(Math.random() * 3) + 1;
+    // click "Save" on a random property
+    within(screen.getByTestId(`result-property-${randomId}`))
+      .getByRole("button", { name: /save/i })
+      .click();
+    // expect the saved property to appear in the document
+    expect(
+      screen.getByTestId(`saved-property-${randomId}`)
+    ).toBeInTheDocument();
   });
 
   test("should correctly remove a property from the saved property list", () => {
-    const { container } = render(<PropertyListings {...properties} />);
-    let saved = container.querySelectorAll(".property-tile-saved");
-    expect(saved.length).toBe(1);
-    // click "Save" on first property
-    screen.getAllByText("Save")[0].click();
-    // get list of saved properties again
-    saved = container.querySelectorAll(".property-tile-saved");
-    // expect saved list to have another property added
-    expect(saved.length).toBe(2);
-    // click "Remove" on first property
-    screen.getAllByText("Remove")[1].click();
-    // get list of saved properties again
-    saved = container.querySelectorAll(".property-tile-saved");
-    // expect saved list to have a property removed
-    expect(saved.length).toBe(1);
+    render(<PropertyListings {...properties} />);
+    const randomId = Math.floor(Math.random() * 3) + 1;
+    // click "Save" on a random property
+    within(screen.getByTestId(`result-property-${randomId}`))
+      .getByRole("button", { name: /save/i })
+      .click();
+    // expect saved list to have the random property added
+    expect(
+      screen.getByTestId(`saved-property-${randomId}`)
+    ).toBeInTheDocument();
+    // click "Remove" on the newly added property
+    within(screen.getByTestId(`saved-property-${randomId}`))
+      .getByRole("button", { name: /remove/i })
+      .click();
+    // expect the saved property to not be in the document anymore
+    expect(
+      screen.queryByTestId(`saved-property-${randomId}`)
+    ).not.toBeInTheDocument();
   });
 });
